b903d82faa7741876b40bb477c9b77f17227dce7
Terence Li <Terence Li>
2017-08-25 12:06:59 -0400

    drag and drop

--- src/app/components/configdragdrop/configdragdrop.component.ts
+++ src/app/components/configdragdrop/configdragdrop.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DragulaDNDService } from '../../services/dragula-dnd.service';
+import { DragulaService } from 'ng2-dragula/ng2-dragula';
 
 @Component({
   selector: 'app-configdragdrop',
@@ -7,57 +8,57 @@ import { DragulaDNDService } from '../../services/dragula-dnd.service';
   styleUrls: ['./configdragdrop.component.css']
 })
 export class ConfigdragdropComponent implements OnInit {
-  constructor() { }
-  // constructor(private dragulaDNDSerivce: DragulaDNDService) {
-  //   dragulaDNDSerivce.drag.subscribe((value) => {
-  //     this.onDrag(value.slice(1));
-  //   });
-  //   dragulaDNDSerivce.drop.subscribe((value) => {
-  //     this.onDrop(value.slice(1));
-  //   });
-  //   dragulaDNDSerivce.over.subscribe((value) => {
-  //     this.onOver(value.slice(1));
-  //   });
-  //   dragulaDNDSerivce.out.subscribe((value) => {
-  //     this.onOut(value.slice(1));
-  //   });
+  constructor(private dragulaSerivce: DragulaService) {
+    dragulaSerivce.drag.subscribe((value) => {
+      this.onDrag(value.slice(1));
+    });
+    dragulaSerivce.drop.subscribe((value) => {
+      this.onDrop(value.slice(1));
+    });
+    dragulaSerivce.over.subscribe((value) => {
+      this.onOver(value.slice(1));
+    });
+    dragulaSerivce.out.subscribe((value) => {
+      this.onOut(value.slice(1));
+    });
+    dragulaSerivce.setOptions('first-bag', {removeOnSpill:true});
 
-  // }
-  // private hasClass(el: any, name: string) {
-  //   return new RegExp('(?:^|\\s+)' + name + '(?:\\s+|$)').test(el.className);
-  // }
+  }
+  private hasClass(el: any, name: string) {
+    return new RegExp('(?:^|\\s+)' + name + '(?:\\s+|$)').test(el.className);
+  }
 
-  // private addClass(el: any, name: string) {
-  //   if (!this.hasClass(el, name)) {
-  //     el.className = el.className ? [el.className, name].join(' ') : name;
-  //   }
-  // }
+  private addClass(el: any, name: string) {
+    if (!this.hasClass(el, name)) {
+      el.className = el.className ? [el.className, name].join(' ') : name;
+    }
+  }
 
-  // private removeClass(el: any, name: string) {
-  //   if (this.hasClass(el, name)) {
-  //     el.className = el.className.replace(new RegExp('(?:^|\\s+)' + name + '(?:\\s+|$)', 'g'), '');
-  //   }
-  // }
+  private removeClass(el: any, name: string) {
+    if (this.hasClass(el, name)) {
+      el.className = el.className.replace(new RegExp('(?:^|\\s+)' + name + '(?:\\s+|$)', 'g'), '');
+    }
+  }
 
-  // private onDrag(args) {
-  //   let [e, el] = args;
-  //   this.removeClass(e, 'ex-moved');
-  // }
+  private onDrag(args) {
+    let [e, el] = args;
+    this.removeClass(e, 'ex-moved');
+  }
 
-  // private onDrop(args) {
-  //   let [e, el] = args;
-  //   this.addClass(e, 'ex-moved');
-  // }
+  private onDrop(args) {
+    let [e, el] = args;
+    this.addClass(e, 'ex-moved');
+  }
 
-  // private onOver(args) {
-  //   let [e, el, container] = args;
-  //   this.addClass(el, 'ex-over');
-  // }
+  private onOver(args) {
+    let [e, el, container] = args;
+    this.addClass(el, 'ex-over');
+  }
 
-  // private onOut(args) {
-  //   let [e, el, container] = args;
-  //   this.removeClass(el, 'ex-over');
-  // }
+  private onOut(args) {
+    let [e, el, container] = args;
+    this.removeClass(el, 'ex-over');
+  }
 
   ngOnInit() {
   }
